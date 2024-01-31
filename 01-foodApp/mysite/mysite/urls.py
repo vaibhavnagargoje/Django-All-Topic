521c49579da0c9136c93db4257770385c171f3de
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-01-31 15:08:31 +0530

    profile photo add or add defalut

--- 01-foodApp/mysite/mysite/urls.py
+++ 01-foodApp/mysite/mysite/urls.py
@@ -22,6 +22,9 @@ from django.urls import path,include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +34,9 @@ urlpatterns = [
     path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('profile/',user_views.profilepage,name='profile'),
 ]
+
+
+
+urlpatterns +=[ 
+    #  the rest of my URLconf goes here 
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
